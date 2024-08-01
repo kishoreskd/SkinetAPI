8258e6050e415a4b6877dac2f7b697b866e60de6
Kishore <142093800+kishoreskd@users.noreply.github.com>
2024-08-01 23:36:30 +0530

    feat: Buggy controller creation.

---
+++ SkinetAPI/Controllers/BuggyController.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SkinetAPI.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly SkiNetDbContext _context;
+
+        public BuggyController(SkiNetDbContext contex)
+        {
+            _context = contex;
+        }
+
+        [HttpGet("testauth")]
+        [Authorize]
+        public ActionResult<string> GetSecretText()
+        {
+            return "secret stuff";
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+
+            if (thing == null) return NotFound();
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
